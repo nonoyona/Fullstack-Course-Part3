a3690d09445c800c407bb16fb6d8d759d37e9a05
Noah- Yannick Schmid <44346907+nonoyona@users.noreply.github.com>
2024-05-11 01:26:57 +0300

    3.8

--- index.js
+++ index.js
@@ -3,29 +3,38 @@ const morgan = require("morgan");
 
 const app = express();
 app.use(express.json())
-app.use(morgan("tiny"));
+
+morgan.token("body", (req, res) => {
+    if (req.method === "POST") {
+        return JSON.stringify(req.body);
+    } else {
+        return " ";
+    }
+});
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"));
 
 
 let data = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
     },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
 ]
 
@@ -63,7 +72,7 @@ app.post("/api/persons", (req, res) => {
     if (data.find(person => person.name === body.name)) {
         return res.status(400).json({ message: "Name must be unique" });
     }
-    let id = Math.floor(Math.random() * 1000000)+1;
+    let id = Math.floor(Math.random() * 1000000) + 1;
     const person = {
         id: id,
         name: body.name,
